ad29426fc6a924af172c8e1fe4c18a4c048da4e3
Alberto Antelo <albertoantelopresa@gmail.com>
2014-04-02 13:11:36 +0200

    creo un arraytradicional con las bolas con parametros aleatorios

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Random;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -11,6 +12,7 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
+  
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -23,15 +25,32 @@ public class BallDemo
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numberOfBalls)
     {
+          Random aleatorio= new Random();//random para crear las bolas
         int ground = 400;   // position of the ground line
 
         myCanvas.setVisible(true);
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
-
+       //creo un array tradicional para guardar las bolas
+       BouncingBall[] bolas = new BouncingBall[numberOfBalls];
+        //comienzo a llenar el array
+        for (int index=0; index < numberOfBalls; index++)
+        {
+         int x = aleatorio.nextInt(250);//me aseguro que sale en la parte izda de la pantalla.
+         int y =  aleatorio.nextInt(410)-10;// y esta entre el suelo y el borde superior.
+         int diam = aleatorio.nextInt(40)+5;//diametro de las bolas
+         //codigo para color aleatorio
+          int red = aleatorio.nextInt(256);
+          int green = aleatorio.nextInt(256);
+          int blue = aleatorio.nextInt(256);
+          Color color = new Color(red, green, blue);
+         //fin color
+        bolas[index] = new BouncingBall(x, y, diam, color, ground, myCanvas);
+        }
+        
         // crate and show the balls
         BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
         ball.draw();
